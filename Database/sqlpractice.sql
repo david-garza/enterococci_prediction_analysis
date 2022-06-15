e1b2e93dabe9220675cfaffa093aa5ec107db2a8
Lauralynn <lauralynn1994@yahoo.com>
2022-06-14 19:44:27 -0500

    initial three table join

--- Database/sqlpractice.sql
+++ Database/sqlpractice.sql
@@ -16,13 +16,14 @@ Two of those results being 0 and null
 that leaves a remainder of 4 snowfall days of the 16983 records */
 
 /* ATTEMPT TO CHANGE THE DATE1 COLUMN NAME IN WEATHER_STATION1 TO DATE*/
-EXECUTE sp_rename 'weather_station1.date1', 'date', 'COLUMN'
+EXECUTE sp_rename 'weather_station1.date1', 'date', 'COLUMN';
 
 /* RIGHT JOIN SO THAT DATA IS EXCLUDED FROM BEACH ATTRIBUTES IF THERE IS NOT A MATCH IN WATER QUALITY, ALL ROWS IN WATER QUALITY WILL BE INCLUDED AND WILL*/
-SELECT beach_attributes.beach_id, beach_attributes.beach_name, start_lat, start_long, end_lat, end_long, waterbody_type, station_id, station_name, bacteria_count, weather_station1.date, avg_temp, max_temp, min_temp, precipitation1
+SELECT beach_attributes.beach_id, beach_attributes.beach_name, start_lat, start_long, end_lat, end_long, waterbody_type, station_id, station_name, bacteria_count, weather_station1.date1, avg_temp1, max_temp1, min_temp1, precipitation1
 FROM beach_attributes
 RIGHT JOIN water_quality 
 ON beach_attributes.beach_id = water_quality.beach_id
 LEFT JOIN weather_station1
 ON water_quality.date = weather_station1.date1 ;
+
 
