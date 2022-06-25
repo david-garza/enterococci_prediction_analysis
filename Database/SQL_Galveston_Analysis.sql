3fe05d9ea46f831b5150db423096b9120062af18
Lauralynn <lauralynn1994@yahoo.com>
2022-06-25 13:53:27 -0500

    examine weather station 18 and 54

--- Database/SQL_Galveston_Analysis.sql
+++ Database/SQL_Galveston_Analysis.sql
@@ -3,8 +3,8 @@
 select * from beach_attributes;
 select * from water_quality;
 select * from weather_station1
-
-
+select * from weather_station18
+select * from weather_station54
 
 select count(distinct snowfall1)
 from weather_station1
@@ -12,8 +12,6 @@ from weather_station1
 Two of those results being 0 and null 
 that leaves a remainder of 4 snowfall days of the 16983 records */
 
-/* ATTEMPT TO CHANGE THE DATE1 COLUMN NAME IN WEATHER_STATION1 TO DATE*/
-EXECUTE sp_rename 'weather_station1.date1', 'date', 'COLUMN';
 
 /* RIGHT JOIN SO THAT DATA IS EXCLUDED FROM BEACH ATTRIBUTES IF THERE IS NOT A MATCH IN WATER QUALITY, ALL ROWS IN WATER QUALITY WILL BE INCLUDED AND WILL*/
 
