b0b167466fb6a39be0305cdaef91316c481103a8
david-garza <locodave@gmail.com>
2022-06-28 20:11:43 -0500

    clean page

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-test = {"message":"Test message to confirm that it is working"}
+test = {"text":"Test message to confirm that it is working"}
 def index():
     return render_template('index.html',test=test)
