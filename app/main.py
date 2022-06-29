1947b185bed38c1272f87984a8199ffd1a512465
david-garza <locodave@gmail.com>
2022-06-28 19:41:43 -0500

    updated list code with pandas data frame

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-test = {"date":['2022-06-23','2022-06-22','2022-06-21']}
-
+d = {"date":['2022-06-23','2022-06-22','2022-06-21']}
+df = pd.DataFrame(data=d)
 @app.route("/")
 def index():
-    return render_template('index.html',test=test)
+    return render_template('index.html',table=df)
