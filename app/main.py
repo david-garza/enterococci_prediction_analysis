44fae3552dc23e5205d346911bae6d8f217ade10
david-garza <locodave@gmail.com>
2022-06-28 19:50:09 -0500

    simple array

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
-import pandas as pd
 
 app = Flask(__name__)
 
 d = {"date":['monday','tuesday','wednesday']}
-df = pd.DataFrame(data=d)
 @app.route("/")
 def index():
-    return render_template('index.html',table=df)
+    return render_template('index.html',table=d)
