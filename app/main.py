5129b526bcf270bd73c34cde4341f58a3019c9df
david-garza <locodave@gmail.com>
2022-06-28 21:43:25 -0500

    list of dictionaries

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
-    return "only flask no rendering"
+    colors = [{"name":"red"},
+        {"name":"green"},
+        {"name":"blue"}]
+    return render_template("index.html",colors=colors)
