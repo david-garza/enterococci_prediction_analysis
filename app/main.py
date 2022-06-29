86e17b8fceaae052299d8a83122f6f624f991d83
david-garza <locodave@gmail.com>
2022-06-28 19:58:58 -0500

    two column dictionary

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-d = {"date":['monday','tuesday','wednesday']}
+days = {"value": [1, 2, 3], "day":['monday','tuesday','wednesday']}
 @app.route("/")
 def index():
-    return render_template('index.html',table=d)
+    return render_template('index.html',days=days)
