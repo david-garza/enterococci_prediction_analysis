f9898de49cdf767461de7670e7f0f7f5839ee822
david-garza <locodave@gmail.com>
2022-06-30 12:35:39 -0500

    Added correct beach drop down menu

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-from unittest import result
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
+
+data = [{'name':'red'}, {'name':'green'}, {'name':'blue'}]
 
 @app.route('/')
 def index():
     return render_template(
         'index.html',
-        dates=[{'name':'red'}, {'name':'green'}, {'name':'blue'}])
+        data=data)
 
 @app.route("/test" , methods=['GET', 'POST'])
 def test():
@@ -15,8 +17,8 @@ def test():
         date_select = request.form.get('date_select')
         beach_select = request.form.get('beach_select')
 
-        response = str(date_select) + ' ' + str(beach_select)
-        
-        return (str(response))
+        response = date_select + ' ' + beach_select
+
+        return (response)
     
     
