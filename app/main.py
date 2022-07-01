2be98a40e199efe58ba9c7cc64e4f3309f015e6a
david-garza <locodave@gmail.com>
2022-06-30 21:03:53 -0500

    henry debug files

--- app/main.py
+++ app/main.py
@@ -8,14 +8,18 @@ def index():
     data = get_dates_list()
     return render_template('index.html',data=data)
 
-@app.route("/predict" , methods=['POST'])
+@app.route("/predict" , methods=['GET','POST'])
 def predict():
-    if request.method == 'POST':
-        date_select = request.form.get('date_select')
-        beach_select = request.form.get('beach_select')
+    #if request.method == 'POST':
+        # date_select = request.form.get('date_select')
+        # beach_select = request.form.get('beach_select')
 
-        response = date_select + ' ' + beach_select
+    request_from_js = request.get_json()
 
-        return (response)
-    
-    
+        # response = {'date':date_select,'station':beach_select}
+        #return render_template('results.html',response=response)
+
+    return request_from_js
+
+if __name__=="__main__":
+    app.run(debug=True)
